937d94909c4d79ffa4125f250d6a0ba016a331a8
Maksim <lisovoi123@gmail.com>
2020-11-24 23:22:37 +0200

    Форматирует код

--- create-gallery.js
+++ create-gallery.js
@@ -1,15 +1,19 @@
-import images from './gallery-items.js';
+import images from "./gallery-items.js";
 
-const gallery = document.querySelector('.js-gallery')
+const gallery = document.querySelector(".js-gallery");
 
-const lightBox = document.querySelector('.js-lightbox')
-const lightBoxOverlay = document.querySelector('.lightbox__overlay') 
-const lightBoxBtn = document.querySelector('[data-action="close-lightbox"]')
-const lightBoxImg = document.querySelector('.lightbox__image')
+const lightBox = document.querySelector(".js-lightbox");
+const lightBoxOverlay = document.querySelector(".lightbox__overlay");
+const lightBoxBtn = document.querySelector('[data-action="close-lightbox"]');
+const lightBoxImg = document.querySelector(".lightbox__image");
 
 // Разметка
 
-const createTags = ({ preview, original, description }) => `<li class="gallery__item">
+const createTags = ({
+  preview,
+  original,
+  description,
+}) => `<li class="gallery__item">
   <a
     class="gallery__link"
     href="${original}"
@@ -23,34 +27,42 @@ const createTags = ({ preview, original, description }) => `<li class="gallery__
   </a>
 </li>`;
 
-  const createCollection = images.reduce((acc, elem) => 
-    acc + createTags(elem),
-   [],)
-gallery.insertAdjacentHTML('afterbegin', createCollection);
-
+const createCollection = images.reduce(
+  (acc, elem) => acc + createTags(elem),
+  ""
+);
+gallery.insertAdjacentHTML("afterbegin", createCollection);
 
 // Обработка событий
 
-
-gallery.addEventListener('click', e => {
-  
+gallery.addEventListener("click", (e) => {
   e.preventDefault();
 
-    if (e.target.classList.contains('gallery__image')) {
-      lightBox.classList.add('is-open')
-      lightBoxImg.src = e.target.dataset.source
-  } if (e.target === lightBoxBtn) {
-     lightBox.classList.remove('is-open')
+  if (e.target.classList.contains("gallery__image")) {
+    lightBox.classList.add("is-open");
+    lightBoxImg.src = e.target.dataset.source;
   }
-})
+  if (e.target === lightBoxBtn) {
+    lightBox.classList.remove("is-open");
+  }
+});
 
 // Закрытие модалки
 
-lightBox.addEventListener('click', e => {
-  if (e.target === lightBoxOverlay || e.target === lightBoxBtn ) {
-    console.log(e.key );
-  lightBox.classList.remove('is-open');
-  lightBoxImg.src = '';
-}
-  
-  })
+lightBox.addEventListener("click", (e) => {
+  if (e.target === lightBoxOverlay || e.target === lightBoxBtn) {
+    lightBox.classList.remove("is-open");
+    lightBoxImg.src = "";
+  }
+});
+document.addEventListener("keydown", (e) => {
+  console.log(e.key);
+  console.log(gallery.childNodes[0]);
+  if (e.key === "Escape") {
+    lightBox.classList.remove("is-open");
+    lightBoxImg.src = "";
+  }
+
+  // if (e.key === "ArrowLeft" || lightBox.classList.contains("is-open")) {
+  // }
+});
